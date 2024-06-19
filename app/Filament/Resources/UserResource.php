1bc0d36aa8c6e7b3fe6f2bbee4c92330380094ba
rchaconalcantara <hola.hack09@gmail.com>
2024-06-19 10:59:04 -0400

    Removing email_verify

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -45,9 +45,6 @@ class UserResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('email_verified_at')
-                    ->dateTime()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('role')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
