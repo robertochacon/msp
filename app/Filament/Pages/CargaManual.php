2fc28722c8f789b8c65a720acd04d4d821e62d94
Starling Daniel Rosario Franco <srosario@aliantek.com.do>
2024-07-04 21:18:53 -0400

    Mejoras en pagina de carga manual

--- app/Filament/Pages/CargaManual.php
+++ app/Filament/Pages/CargaManual.php
@@ -15,113 +15,93 @@ class CargaManual extends Page
 
     protected ?string $subheading = 'Carga manual de datos del sistema financiero hacia Pali Web.';
 
+    protected function showPageHeaderActions(): bool
+    {
+        return false;
+    }
+
     protected function getActions(): array
     {
         return [
-            Action::make('Historial de carga')
-                ->icon('heroicon-o-eye')
-                ->url('bitacoras')
+            Action::make('load_clients')
+                ->label('Cargar clientes')
+                ->requiresConfirmation()
+                ->icon('heroicon-s-arrow-path')
+                ->action(function(){
+    
+                    try {
+    
+                        Artisan::call('app:data-clients');
+    
+                        Notification::make()
+                        ->title('Carga completa.')
+                        ->success()
+                        ->send();
+    
+                    } catch (\Throwable $th) {
+    
+                        Notification::make()
+                        ->title('Error al cargar clientes.')
+                        ->danger()
+                        ->send();
+    
+                    }
+    
+                }),
+
+            Action::make('load_loans')
+                ->label('Cargar creditos')
+                ->requiresConfirmation()
+                ->icon('heroicon-s-arrow-path')
+                ->action(function(){
+    
+                    try {
+ 
+                        Artisan::call('app:data-crdits');
+    
+                        Notification::make()
+                        ->title('Carga completa.')
+                        ->success()
+                        ->send();
+    
+                    } catch (\Throwable $th) {
+    
+                        Notification::make()
+                        ->title('Error al cargar creditos.')
+                        ->danger()
+                        ->send();
+    
+                    }
+    
+                }),
+
+            Action::make('load_loans_movements')
+                ->label('Cargar movimientos')
+                ->requiresConfirmation()
+                ->icon('heroicon-s-arrow-path')
+                ->action(function(){
+    
+                    try {
+    
+                        Artisan::call('app:data-movements');
+    
+                        Notification::make()
+                        ->title('Carga completa.')
+                        ->success()
+                        ->send();
+    
+                    } catch (\Throwable $th) {
+    
+                        Notification::make()
+                        ->title('Error al cargar movimientos.')
+                        ->danger()
+                        ->send();
+    
+                    }
+    
+                })
+
         ];
     }
-
-    public function cargaClients(): Action
-    {
-        return Action::make('Cargar clientes')
-            ->requiresConfirmation()
-            ->icon('heroicon-s-arrow-path')
-            ->action(function(){
-
-                try {
-
-                    Notification::make()
-                    ->title('Carga de clientes en proceso')
-                    ->info()
-                    ->send();
-
-                    Artisan::call('app:data-clients');
-
-                    Notification::make()
-                    ->title('Carga completa.')
-                    ->success()
-                    ->send();
-
-                } catch (\Throwable $th) {
-
-                    Notification::make()
-                    ->title('Error al cargar clientes.')
-                    ->danger()
-                    ->send();
-
-                }
-
-            });
-    }
-
-    public function cargaCredits(): Action
-    {
-        return Action::make('Cargar creditos')
-            ->requiresConfirmation()
-            ->icon('heroicon-s-arrow-path')
-            ->action(function(){
-
-                try {
-
-                    Notification::make()
-                    ->title('Carga de creditos en proceso')
-                    ->info()
-                    ->send();
-
-                    Artisan::call('app:data-crdits');
-
-                    Notification::make()
-                    ->title('Carga completa.')
-                    ->success()
-                    ->send();
-
-                } catch (\Throwable $th) {
-
-                    Notification::make()
-                    ->title('Error al cargar creditos.')
-                    ->danger()
-                    ->send();
-
-                }
-
-            });
-    }
-
-    public function cargaMovements(): Action
-    {
-        return Action::make('Cargar movimientos')
-            ->requiresConfirmation()
-            ->icon('heroicon-s-arrow-path')
-            ->action(function(){
-
-                try {
-
-                    Notification::make()
-                    ->title('Carga de movimientos en proceso')
-                    ->info()
-                    ->send();
-
-                    Artisan::call('app:data-movements');
-
-                    Notification::make()
-                    ->title('Carga completa.')
-                    ->success()
-                    ->send();
-
-                } catch (\Throwable $th) {
-
-                    Notification::make()
-                    ->title('Error al cargar movimientos.')
-                    ->danger()
-                    ->send();
-
-                }
-
-            });
-    }
-
 
 }
--- resources/views/filament/pages/carga-manual.blade.php
+++ resources/views/filament/pages/carga-manual.blade.php
@@ -1,5 +1,13 @@
 <x-filament-panels::page>
 
+@push('styles')
+<style>
+    .fi-header .fi-ac {
+        display: none !important;
+    }
+</style>
+@endpush
+
 <hr>
 
 <div style="display: flex;flex-wrap:wrap;justify-content:space-around;">
@@ -7,7 +15,7 @@
     <div>
         <center>
             <h2><b>Carga de clientes</b></h2><hr><br>
-            {{ $this->cargaClients }}
+            {{ $this->getAction('load_clients') }}
             <img src="https://st2.depositphotos.com/5266903/11817/v/450/depositphotos_118171126-stock-illustration-staff-flat-vector-icon.jpg" width="150" alt="">
         </center>
     </div>
@@ -15,7 +23,7 @@
     <div>
         <center>
             <h2><b>Carga de Creditos</b></h2><hr><br>
-            {{ $this->cargaCredits }}
+            {{ $this->getAction('load_loans') }}
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1RkEoSgQnRmnpMZ-35-ETmFyuTyKfn36-MA&s" width="150" alt="">
         </center>
     </div>
@@ -23,7 +31,7 @@
     <div>
         <center>
             <h2><b>Carga de Movimientos</b></h2><hr><br>
-            {{ $this->cargaMovements }}
+            {{ $this->getAction('load_loans_movements') }}
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFpWIfOuB_xiitU9KPkD7ZOtGK-cgW9zObtA&s" width="150" alt="">
         </center>
     </div>
